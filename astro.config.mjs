3f2bfb67065ac82f67bc999f7df4eb81a39536df
Daniel M.C. <alinedmooner@gmail.com>
2026-04-24 21:28:44 -0500

    feat: deploy on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://alinedmooner.github.io",
+	base: "/midu-portfolio",
 	integrations: [sitemap(), robotsTxt()]
 });
